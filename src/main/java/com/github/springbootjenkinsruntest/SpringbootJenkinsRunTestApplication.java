6ae2b4fb50bfe44a0da98df7bf2676279164cc54
huhuhuHR <757033301@qq.com>
2023-01-15 10:16:28 +0800

    huhuhuhr
    init

---
+++ src/main/java/com/github/springbootjenkinsruntest/SpringbootJenkinsRunTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.springbootjenkinsruntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsRunTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsRunTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springbootjenkinsruntest/SpringbootJenkinsRunTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springbootjenkinsruntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsRunTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
